3bd0ae331877d3af07dd80c995791a6d9198d08c
Alvaro Robles <roblesgg16@gmail.com>
2025-05-20 17:20:10 +0200

    Cambios

---
+++ src/controller/LoginController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class LoginController {
+
+    @FXML
+    private Button btnEntrar;
+
+    @FXML
+    private ImageView imgFondo;
+
+    @FXML
+    private ImageView imgLogo;
+
+    @FXML
+    void abrirMenu(ActionEvent event) {
+
+    }
+
+}
