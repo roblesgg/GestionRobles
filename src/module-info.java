69448bcaeb88f67ed771ecdac0116caf62e7d58f
Alvaro Robles <roblesgg16@gmail.com>
2025-05-20 22:29:18 +0200

    Menu.fxml y controller

--- src/module-info.java
+++ src/module-info.java
@@ -8,6 +8,7 @@ module GestionRobles {
 	requires javafx.fxml;
 	requires javafx.base;
 	requires javafx.controls;
+	requires javafx.graphics;
     exports main;
     opens controller to javafx.fxml;
 
